672776acd3850a56596800d53af0d3559a3c55cf
Evan Schoening <evan.schoening@gmail.com>
2022-02-09 09:39:25 -0800

    search function passing test

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,18 @@ pub fn run(config: Config) -> Result<String, Box<dyn Error>> {
 }
 // this function will need to implement lifetimes
 // contents will only have lifetime of this function in order to search
-pub fn search(query: &str, contents: &str) -> Vec<_> {
-    vec![]
+// need lifetimes here to reference borrowing
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut t = Vec::new();
+    for i in contents.lines() {
+       // println!(" new line{}", i);
+        if i.contains(query) {
+            t.push(i);
+            println!("{:?}", t)
+        }
+        
+    }
+    t
 }
 
 // test configuration 
@@ -38,12 +48,13 @@ mod tests {
     #[test]
     fn initial() {
         let query_test = "chain";
-        let contents_test = "rust is a great lang to learn
-        it has memory safe concepts
-        it is performant
-        used in blockchain";
+        let contents_test = 
+        "rust is a great lang to learn
+it has memory safe concepts
+it is performant
+used in blockchain";
         assert_eq!(
-            vec!["used", "in", "blockchain"],
+            vec!["used in blockchain"],
             search(&query_test, &contents_test)
         )
 
